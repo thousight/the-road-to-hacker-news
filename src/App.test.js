797758f19670bb9e45c100fe839ba84997f5993a
Mark Wen <markwenguojie94@gmail.com>
2018-07-17 14:58:56 -0400

    Added enzyme test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
 import App from './App'
 import Button from './components/Button'
 import Search from './components/Search'
 import Table from './components/Table'
+
+Enzyme.configure({ adapter: new Adapter() })
 
 describe('App', () => {
 
@@ -68,22 +72,30 @@ describe('Table', () => {
       { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
       { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
     ],
-    onBottomVisible: () => {},
-    onDismiss: () => {},
+    onBottomVisible: () => { },
+    onDismiss: () => { },
     isAtEnd: true
   }
 
   it('renders without crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<Table { ...props } />, div)
+    ReactDOM.render(<Table {...props} />, div)
   })
 
   test('has a valid snapshot', () => {
     const component = renderer.create(
-      <Table { ...props } />
+      <Table {...props} />
     )
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot()
+  })
+
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table {...props} />
+    );
+
+    expect(element.find('.table-row').length).toBe(2);
   })
 
 })
