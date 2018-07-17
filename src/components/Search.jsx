886c664f8f093516d326cc0918e9d84eed533f0a
Mark Wen <markwenguojie94@gmail.com>
2018-07-17 15:32:58 -0400

    Put focus on search at load

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-const Search = ({ value, onChange, onSubmit, children }) => (
-    <form onSubmit={onSubmit}>
-        <input
-            placeholder={children}
-            type="text"
-            value={value}
-            onChange={onChange} />
-        <button type="submit">
-            {children}
-        </button>
-    </form>
-)
+class Search extends Component {
+    componentDidMount() {
+        if (this.input) {
+            this.input.focus()
+        }
+    }
+
+    render() {
+        const {
+            value,
+            onChange,
+            onSubmit,
+            children
+        } = this.props
+
+        return (
+            <form onSubmit={onSubmit}>
+                <input type="text"
+                    value={value}
+                    onChange={onChange}
+                    ref={node => this.input = node} />
+                <button type="submit">
+                    {children}
+                </button>
+            </form>
+        )
+    }
+}
 
 Search.propTypes = {
     value: PropTypes.string.isRequired,
