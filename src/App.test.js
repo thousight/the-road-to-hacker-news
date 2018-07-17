06994135be74b47d391af573349b1a3ccfb81558
Mark Wen <markwenguojie94@gmail.com>
2018-07-17 14:22:40 -0400

    Added jest test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,89 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import renderer from 'react-test-renderer'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+import App from './App'
+import Button from './components/Button'
+import Search from './components/Search'
+import Table from './components/Table'
+
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <App />
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+})
+
+describe('Search', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Search>Search</Search>, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Search>Search</Search>
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+})
+
+describe('Button', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Button>Give Me More</Button>, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Button>Give Me More</Button>
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+})
+
+describe('Table', () => {
+
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+    ],
+    onBottomVisible: () => {},
+    onDismiss: () => {},
+    isAtEnd: true
+  }
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Table { ...props } />, div)
+  })
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Table { ...props } />
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
 })
