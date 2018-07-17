86571a6958a180036e6a59adc701abf91679be88
Mark Wen <markwenguojie94@gmail.com>
2018-07-17 11:07:56 -0400

    Added client caching algorithms

--- src/App.jsx
+++ src/App.jsx
@@ -8,61 +8,91 @@ import { HACKERNEWS_API_PATH } from './constants/strings'
 import { HACKERNEWS_API_SEARCH_RESULT_COUNT } from './constants/numbers'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      searchTerm: '',
-      result: [],
-      page: 0,
-      isAtEnd: false
-    }
-
-    this.onSearchChange = this.onSearchChange.bind(this)
-    this.onSearchSubmit = this.onSearchSubmit.bind(this)    
-    this.onDismiss = this.onDismiss.bind(this)
-    this.setSearchNewsResult = this.setSearchNewsResult.bind(this)
-    this.handleHackerNewsFetch = this.handleHackerNewsFetch.bind(this)
+  state = {
+    searchTerm: '',
+    searchKey: '',
+    result: {}
   }
+  onSearchChange = this.onSearchChange.bind(this)
+  onSearchSubmit = this.onSearchSubmit.bind(this)
+  onDismiss = this.onDismiss.bind(this)
+  setSearchNewsResult = this.setSearchNewsResult.bind(this)
+  handleHackerNewsFetch = this.handleHackerNewsFetch.bind(this)
 
   onSearchChange(event) {
-    this.setState({ searchTerm: event.target.value, page: 0 })
+    this.setState({ searchTerm: event.target.value })
   }
 
   onSearchSubmit(event) {
     event.preventDefault()
-    this.setState({ result: [] }, () => {
-      this.handleHackerNewsFetch(this.state.searchTerm)
+    const { searchTerm, result } = this.state
+    this.setState({
+      searchKey: searchTerm
+    }, () => {
+      if (!result[searchTerm]) {
+        this.handleHackerNewsFetch()
+      }
     })
   }
 
   onDismiss(id) {
+    const { result, searchKey } = this.state
     this.setState({
-      result: this.state.result.filter(item => item.objectID !== id)
+      result: {
+        ...result,
+        [searchKey]: {
+          ...result[searchKey],
+          hits: result[searchKey].hits.filter(item => item.objectID !== id)
+        }
+      }
     })
   }
 
-  setSearchNewsResult(result, page, isAtEnd) {
-    this.setState({ result: this.state.result.concat(result), page, isAtEnd })
+  setSearchNewsResult(result) {
+    const { searchKey } = this.state
+
+    if (this.state.result[searchKey]) {
+      // If searchKey already exist
+      this.setState({
+        result: {
+          ...this.state.result,
+          [this.state.searchKey]: {
+            ...result,
+            hits: this.state.result[this.state.searchKey].hits.concat(result.hits),
+          }
+        }
+      })
+    } else {
+      // If this is a new search key
+      this.setState({
+        result: {
+          ...this.state.result,
+          [this.state.searchKey]: result
+        }
+      })
+    }
   }
 
   handleHackerNewsFetch() {
-    let { searchTerm, page } = this.state
-    if (searchTerm && searchTerm.length > 0) {
-      axios.get(`${HACKERNEWS_API_PATH}/search?query=${searchTerm}&page=${page}&hitsPerPage=${HACKERNEWS_API_SEARCH_RESULT_COUNT}`)
-      .then(res => {
-        this.setSearchNewsResult(res.data.hits, ++page, res.data.nbPages <= 0)
-      })
-      .catch(error => {
-        console.log(error.response)
-      })
+    const { searchKey, result } = this.state
+
+    if (searchKey && searchKey.length > 0) {
+      axios.get(`${HACKERNEWS_API_PATH}/search?query=${searchKey}&page=${result[searchKey] ? ++result[searchKey].page : 0}&hitsPerPage=${HACKERNEWS_API_SEARCH_RESULT_COUNT}`)
+        .then(res => {
+          this.setSearchNewsResult(res.data)
+        })
+        .catch(error => {
+          console.log(error.response)
+        })
     } else {
-      this.setSearchNewsResult(null, 0)
+      this.setSearchNewsResult({})
     }
   }
 
   render() {
-    const { searchTerm, result, isAtEnd } = this.state
+    const { searchTerm, searchKey, result } = this.state
+    let data = result[searchKey]
 
     return (
       <div className="page">
@@ -75,14 +105,14 @@ class App extends Component {
           </Search>
 
           {
-            result && result.length > 0
+            result && data && data.hits
               ?
               <Table
-                list={result}
+                list={data.hits}
                 onBottomVisible={this.handleHackerNewsFetch}
-                isAtEnd={isAtEnd}
+                isAtEnd={data.nbPages <= 0 || (data.nbPages - 1) === data.page}
                 onDismiss={this.onDismiss} />
-              : 
+              :
               null
           }
 
