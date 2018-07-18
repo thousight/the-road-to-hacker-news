41d1b2078f0ee824b3d8bf911f2d94a8e53638d2
Mark Wen <markwenguojie94@gmail.com>
2018-07-18 13:47:11 -0400

    Added antd to Table and responsive design

--- src/components/DismissButton.jsx
+++ src/components/DismissButton.jsx
@@ -2,16 +2,17 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Button } from "antd"
 
-const DismissButton = ({ onClick, className = "", children }) => (
-  <Button className={className} type="danger" shape="circle" onClick={onClick}>
-    {children}
-  </Button>
+const DismissButton = ({ onClick, className = "" }) => (
+  <Button className={className}
+    type="danger"
+    shape="circle"
+    icon="close"
+    onClick={onClick} />
 )
 
 DismissButton.propTypes = {
   onClick: PropTypes.func.isRequired,
-  className: PropTypes.string,
-  children: PropTypes.node.isRequired
+  className: PropTypes.string
 }
 
 export default DismissButton
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,52 +1,85 @@
-import React from "react"
+import React, { Component } from "react"
 import VisibilitySensor from "react-visibility-sensor"
 import { PulseLoader } from "react-spinners"
 import PropTypes from "prop-types"
-import { Icon } from "antd"
+import { Table, Icon } from "antd"
 
 import "../styles/Table.css"
 
 import DismissButton from "./DismissButton"
 import { primaryOrange } from "../constants/colors"
 
-const Table = ({ list, onDismiss, onBottomVisible, isAtEnd }) => (
-  <div className="table">
-    {list.map(item => (
-      <div key={item.objectID} className="table-row">
-        <span style={{ width: "40%" }}>
-          <a href={item.url} target="_blank">{item.title}</a>
-        </span>
-        <span style={{ width: "30%" }}>{item.author}</span>
-        <span style={{ width: "10%" }}>{item.num_comments}</span>
-        <span style={{ width: "10%" }}>{item.points}</span>
-        <span style={{ width: "10%" }}>
-          <DismissButton onClick={() => onDismiss(item.objectID)}>
-            <Icon type="close" />
-          </DismissButton>
-        </span>
+class ResultsTable extends Component {
+
+  columns = [{
+    title: this.renderHeaderTitle('Title', 'file-text'),
+    dataIndex: 'title',
+    key: 'title',
+    width: '40%',
+    render: (title, item) => <a href={item.url} target="_blank">{title}</a>
+  }, {
+    title: this.renderHeaderTitle('Author', 'user'),
+    dataIndex: 'author',
+    key: 'author',
+    width: '20%'
+  }, {
+    title: this.renderHeaderTitle('Points', 'like-o'),
+    dataIndex: 'points',
+    key: 'points',
+    width: '15%'
+  }, {
+    title: this.renderHeaderTitle('Comments', 'message'),
+    dataIndex: 'num_comments',
+    key: 'num_comments',
+    width: '15%'
+  }, {
+    title: this.renderHeaderTitle('Action', 'ellipsis'),
+    key: 'objectID',
+    width: '10%',
+    render: item => <DismissButton onClick={() => this.props.onDismiss(item.objectID)} />
+  }]
+
+  renderHeaderTitle(title, icon) {
+    return (
+      <div className="table-header">
+        <Icon type={icon} />
+        <span>{title}</span>
       </div>
-    ))}
+    )
+  }
 
-    <div className="loading-spinner-wrapper">
-      <VisibilitySensor
-        active={!isAtEnd}
-        onChange={isVisible => (isVisible ? onBottomVisible() : null)}
-      >
-        {isAtEnd ? (
-          <p>All results are loaded</p>
-        ) : (
-          <PulseLoader color={primaryOrange} />
-        )}
-      </VisibilitySensor>
-    </div>
-  </div>
-)
+  render() {
+    const { list, onBottomVisible, isAtEnd } = this.props
 
-Table.propTypes = {
+    return (
+      <div className="table">
+        <Table columns={this.columns}
+          dataSource={list}
+          rowKey="objectID"
+          pagination={false} />
+
+        <div className="loading-spinner-wrapper">
+          <VisibilitySensor
+            active={!isAtEnd}
+            onChange={isVisible => (isVisible ? onBottomVisible() : null)}
+          >
+            {isAtEnd ? (
+              <p>All results are loaded</p>
+            ) : (
+                <PulseLoader color={primaryOrange} />
+              )}
+          </VisibilitySensor>
+        </div>
+      </div>
+    )
+  }
+}
+
+ResultsTable.propTypes = {
   list: PropTypes.array.isRequired,
   onDismiss: PropTypes.func.isRequired,
   onBottomVisible: PropTypes.func.isRequired,
   isAtEnd: PropTypes.bool.isRequired
 }
 
-export default Table
+export default ResultsTable
